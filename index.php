f1b1c7e86789baf25e94bcd437ae0ef71c099cee
--replace <roberto.bellardigiol@unito.it>
2019-11-17 18:44:33 +0100

    Inizio sito personale

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="it">
+<?php include "html/head.html" ?>
+
+<body>
+
+<?php include "html/header.html"?>
+
+<?php include "html/center.html"?>
+
+<?php include "html/footer.html"?>
+
+
+
+
+</body>
+</html>
