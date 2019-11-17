3aa6208f7d3ffedf28d46464676a45ea2fa22a30
--replace <roberto.bellardigiol@unito.it>
2019-11-17 21:05:44 +0100

    Inizio sito personale

--- js/customJs.js
+++ js/customJs.js
@@ -1,3 +1,9 @@
 window.onload=function(){
+    $("#linkMail").on("click",openMail);
+}
 
+function openMail(){
+    console.log("Stampo");
+    window.location.href = "mailto:roberto.bellardi94@gmail.com?subject=Contatto tramite sito " +
+        "perdonale&body=";
 }
