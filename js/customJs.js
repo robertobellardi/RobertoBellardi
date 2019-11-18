a22735be9eb19ae9afdc60aa010c460925643202
--replace <roberto.bellardigiol@unito.it>
2019-11-18 18:43:41 +0100

    bug fix e modifiche layout di pagina

--- js/customJs.js
+++ js/customJs.js
@@ -1,9 +1,24 @@
+var openMenu=false;
+
 window.onload=function(){
     $("#linkMail").on("click",openMail);
+    $(".navbar-toggler-icon").on("click",checkOpenMenu);
 }
 
 function openMail(){
     console.log("Stampo");
     window.location.href = "mailto:roberto.bellardi94@gmail.com?subject=Contatto tramite sito " +
         "perdonale&body=";
+}
+
+function checkOpenMenu(){
+    console.log("Entro");
+    if(!openMenu){
+        $("#center").css("margin-top","250px");
+        openMenu=true;
+    }
+    else {
+        $("#center").css("margin-top", "100px");
+        openMenu=false;
+    }
 }
